74ec25281c41daa8b6021f90b7d06ab46f115065
Anshuman Mohanty <89729157+ansty07@users.noreply.github.com>
2025-08-06 17:28:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -489,7 +489,7 @@ st.markdown("""
 ### 🎵 Stress Reliever Song
 Listen to "Weightless" by Marconi Union, a scientifically recognized stress-relief track:
 
-<iframe width="100%" height="120" src="https://youtu.be/RCMXO9sBIcU?si=DQt5TcPuQh94YQLZ" title="Everdream by Epic Soul Factory" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+<iframe width="100%" height="120" src="https://youtu.be/P6r0XloK9tw?si=9oFA4lr-TC3v4SKi" title="Everdream by Epic Soul Factory" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 """, unsafe_allow_html=True)
 
 st.markdown("""
@@ -521,6 +521,7 @@ If you're experiencing thoughts of self-harm or severe crisis:<br>
 
 
 st.balloons()
+
 
 
 
